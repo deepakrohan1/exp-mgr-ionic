e0eb635ba2333a5f40899c3986676cbcddcae0a4
rohan <deepakrohan@hotmail.com>
2021-11-17 19:39:44 -0500

    adding menu and slider to categories

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { Category } from './category.model';
 import { CategoryService } from './category.service';
 
@@ -11,7 +13,8 @@ export class CategoryPage implements OnInit, OnDestroy {
 
   categories: Category[] = [];
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -29,6 +32,12 @@ export class CategoryPage implements OnInit, OnDestroy {
 
   ionViewDidEnter() {
     console.log('ionViewDidEnter CategoryPage');
+  }
+
+  updateCategory(categoryId: string, slidingCat: IonItemSliding) {
+    slidingCat.close();
+    console.log('update category: ' + categoryId);
+    this.router.navigate(['/', 'categories', categoryId]);
   }
 
   ngOnDestroy() {
